be6968c834579665a03fcea52b8e37b5445081f1
Nessa <la-nessa@mail.ru>
2022-12-16 00:37:23 +0300

    Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿int [] CreateRandomArray(int length)
+{
+    int [] array = new int [length];
+    Random N = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = N.Next(-100, 100);
+        Console.WriteLine(array[i]);
+    }
+    return array;
+}
+
+int [] arr1 = CreateRandomArray(5);
+
+int sum = 0;
+for (int i = 0; i < 5; i++)
+{
+    if (i % 2 == 0)
+    {
+        sum = sum + 0;
+    }
+    else sum = sum + arr1[i];
+}
+Console.Write("Сумма элементов на нечетных позициях: ");
+Console.WriteLine(sum);
